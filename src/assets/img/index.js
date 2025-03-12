a7b5771662cbe5929b4c4a7f43d4208da6f0f4c8
Alan Gomez <alangmvaz@gmail.com>
2025-03-12 01:02:04 -0600

    chore: se crea archivo de barril para imagenes

---
+++ src/assets/img/index.js
@@ -0,0 +1,6 @@
+export { default as buttonDelete } from "./delete.svg";
+export { default as buttonAdd } from "./create.svg";
+export { default as buttoLink } from "./link.svg";
+export { default as buttonChart } from "./chart.svg";
+export { default as buttonSave } from "./save.svg";
+export { default as noChanges } from "./changes.svg";
