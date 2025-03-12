5e2bf5ffb12d2c0489bf992019eaa24b92e02399
Alan Gomez <alangmvaz@gmail.com>
2025-03-12 01:31:56 -0600

    chore agregar archivo de barril para componentes

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { DeleteSurveyModal } from "../components/DeleteSurveyModal";
+export { SurveyHeader } from "../components/SurveyHeader";
+export { SurveyQuestions } from "../components/SurveyQuestions";
+export { SurveyActions } from "../components/SurveyActions";
